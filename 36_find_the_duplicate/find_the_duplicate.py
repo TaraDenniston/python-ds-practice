124ffc332e3f3faf3c0879820bc88b6f81b5b523
Tara Denniston <taradenniston@gmail.com>
2022-10-20 19:12:57 -0400

    completed find_the_duplicate exercise

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,13 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+    tracked = []
+
+    for num in nums:
+        if num not in tracked:
+            tracked.append(num)
+        elif num in tracked:
+            return num
+    
+    
