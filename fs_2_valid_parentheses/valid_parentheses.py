605b899cc554eaa942d55711f2d3c9e6ece8081a
Tara Denniston <taradenniston@gmail.com>
2022-10-22 11:38:17 -0400

    completed valid_parentheses exercise

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,19 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+
+    if len(parens) % 2 == 0:
+        if parens[0] == '(' and parens[-1] == ')':
+            for i in range(1, len(parens) - 1):
+                if parens[i] == '(' and parens[-i - 1] == ')' or \
+                   parens[i] == ')' and parens[-i - 1] == '(':
+                   continue
+                else:
+                    return False
+        else:
+            return False
+    else:
+        return False 
+
+    return True
+
