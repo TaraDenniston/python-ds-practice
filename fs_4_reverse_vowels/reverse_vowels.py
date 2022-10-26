32f0d255cfbadb5b0bb9e5197bc6a0b45b213cac
Tara Denniston <taradenniston@gmail.com>
2022-10-25 20:00:10 -0400

    completed reverse_vowels exercise

--- fs_4_reverse_vowels/reverse_vowels.py
+++ fs_4_reverse_vowels/reverse_vowels.py
@@ -19,3 +19,28 @@ def reverse_vowels(s):
     reverse_vowels("why try, shy fly?")
     'why try, shy fly?''
     """
+    
+    new_s = ''
+
+    # turn string into dictionary using index numbers as keys
+    s_dict = dict(enumerate(s))
+
+    # extract all key/value pairs that contain a vowel
+    vowels = {k:v for (k,v) in s_dict.items() if v in 'AaEeIiOoUu'}
+
+    # separate key/value pairs into two lists, reverse the order of the vowels, 
+    # and put them back together into a dictionary
+    keys = list(vowels.keys())
+    values = list(vowels.values())
+    values = values[::-1]
+    vowels = dict(zip(keys, values))
+
+    # update original dictionary with the new order of vowels
+    s_dict.update(vowels)
+
+    # turn dictionary back into string
+    for val in s_dict.values():
+        new_s += val
+
+    return new_s
+
